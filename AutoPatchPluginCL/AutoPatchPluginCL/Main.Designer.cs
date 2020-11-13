7d5963c8890934e1d0681852957498929c31a26b
DaRkFoxDeveloper <darkfoxdeveloper@gmail.com>
2020-11-13 20:26:25 +0100

    All working for the first beta

--- AutoPatchPluginCL/AutoPatchPluginCL/Main.Designer.cs
+++ AutoPatchPluginCL/AutoPatchPluginCL/Main.Designer.cs
@@ -29,40 +29,71 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.pBarProgress = new System.Windows.Forms.ProgressBar();
             this.lblCurrentVer = new System.Windows.Forms.Label();
+            this.bgWorkerAutoPatch = new System.ComponentModel.BackgroundWorker();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnPlay = new MetroFramework.Controls.MetroButton();
             this.SuspendLayout();
             // 
-            // progressBar1
+            // pBarProgress
             // 
-            this.progressBar1.Location = new System.Drawing.Point(20, 225);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(680, 41);
-            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
-            this.progressBar1.TabIndex = 0;
+            this.pBarProgress.Location = new System.Drawing.Point(34, 143);
+            this.pBarProgress.Name = "pBarProgress";
+            this.pBarProgress.Size = new System.Drawing.Size(673, 33);
+            this.pBarProgress.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.pBarProgress.TabIndex = 0;
             // 
             // lblCurrentVer
             // 
             this.lblCurrentVer.AutoSize = true;
             this.lblCurrentVer.Font = new System.Drawing.Font("Caladea", 9.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCurrentVer.Location = new System.Drawing.Point(31, 90);
+            this.lblCurrentVer.Location = new System.Drawing.Point(582, 30);
             this.lblCurrentVer.Name = "lblCurrentVer";
-            this.lblCurrentVer.Size = new System.Drawing.Size(148, 23);
+            this.lblCurrentVer.Size = new System.Drawing.Size(104, 16);
             this.lblCurrentVer.TabIndex = 1;
             this.lblCurrentVer.Text = "Current version: ";
             // 
+            // bgWorkerAutoPatch
+            // 
+            this.bgWorkerAutoPatch.WorkerReportsProgress = true;
+            this.bgWorkerAutoPatch.WorkerSupportsCancellation = true;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Caladea", 9.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(31, 112);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(93, 16);
+            this.lblStatus.TabIndex = 2;
+            this.lblStatus.Text = "Downloading...";
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Enabled = false;
+            this.btnPlay.Location = new System.Drawing.Point(321, 182);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(150, 35);
+            this.btnPlay.TabIndex = 3;
+            this.btnPlay.Text = "Launch ConquerLoader";
+            this.btnPlay.UseSelectable = true;
+            this.btnPlay.Click += new System.EventHandler(this.BtnPlay_Click);
+            // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(711, 287);
+            this.ClientSize = new System.Drawing.Size(728, 225);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.lblStatus);
             this.Controls.Add(this.lblCurrentVer);
-            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pBarProgress);
             this.Font = new System.Drawing.Font("Caladea", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Main";
-            this.Padding = new System.Windows.Forms.Padding(18, 57, 18, 19);
+            this.Padding = new System.Windows.Forms.Padding(18, 60, 18, 19);
             this.Resizable = false;
             this.Text = "AutoPatch for ConquerLoader";
             this.Load += new System.EventHandler(this.Main_Load);
@@ -73,8 +104,11 @@
 
         #endregion
 
-        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar pBarProgress;
         private System.Windows.Forms.Label lblCurrentVer;
+        private System.ComponentModel.BackgroundWorker bgWorkerAutoPatch;
+        private System.Windows.Forms.Label lblStatus;
+        private MetroFramework.Controls.MetroButton btnPlay;
     }
 }
 
--- AutoPatchPluginCL/AutoPatchPluginCL/Main.cs
+++ AutoPatchPluginCL/AutoPatchPluginCL/Main.cs
@@ -8,8 +8,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Threading;
-using System.Windows.Forms;
 
 namespace AutoPatch
 {
@@ -22,9 +20,13 @@ namespace AutoPatch
 
         private AutoPatchConfig AutoPatchConfig = null;
         private PatchList PatchList = null;
+        private readonly WebClient client = new WebClient();
+        private bool AnyPatchApplied = false;
 
         private void Main_Load(object sender, EventArgs e)
         {
+            // Start Autopatcher
+
             if (File.Exists("AutoPatchPluginCLConfig.json"))
             {
                 AutoPatchConfig = JsonConvert.DeserializeObject<AutoPatchConfig>(File.ReadAllText("AutoPatchPluginCLConfig.json"));
@@ -39,58 +41,97 @@ namespace AutoPatch
                 AutoPatchConfig = config;
             }
             lblCurrentVer.Text = "Current version: " + AutoPatchConfig.CurrentVersion.ToString();
-            WebClient client = new WebClient();
             if (ValidURL(AutoPatchConfig.PatchListUrl))
             {
-                // Download the patchlist from specified url
-                client.DownloadFile(AutoPatchConfig.PatchListUrl, "PatchList.json");
-                Uri PatchListURI = new Uri(AutoPatchConfig.PatchListUrl);
-                PatchList = JsonConvert.DeserializeObject<PatchList>(File.ReadAllText("PatchList.json"));
-                if (PatchList.CurrentVersion > AutoPatchConfig.CurrentVersion)
-                {
-                    List<Patch> patchs = PatchList.Paths.Where(x => x.Version > AutoPatchConfig.CurrentVersion).OrderBy(x => x.Version).ToList();
-                    List<string> FilesToClear = new List<string>();
-                    foreach (Patch p in patchs)
-                    {
-                        string fileNameRar = "Patch" + p.Version + ".rar";
-                        client.DownloadFile(PatchListURI.Scheme + "://" + PatchListURI.Host + "/" + p.RelativeURL, fileNameRar);
-                        if (File.Exists(fileNameRar))
-                        {
-                            var archive = ArchiveFactory.Open(fileNameRar);
-                            foreach (var entry in archive.Entries)
-                            {
-                                if (!entry.IsDirectory)
-                                {
-                                    entry.WriteToDirectory(Environment.CurrentDirectory, new ExtractionOptions() { ExtractFullPath = true, Overwrite = true });
-                                }
-                            }
-                            ApplyNewVersionAndReloadVersion();
-                            FilesToClear.Add(fileNameRar);
-                            archive.Dispose();
-                        }
-                    }
-                    foreach (string filename in FilesToClear)
-                    {
-                        File.Delete(filename);
-                    }
-                    Environment.Exit(0);
-                }
+                bgWorkerAutoPatch.DoWork += BgWorkerAutoPatch_DoWork;
+                bgWorkerAutoPatch.ProgressChanged += BgWorkerAutoPatch_ProgressChanged;
+                bgWorkerAutoPatch.RunWorkerCompleted += BgWorkerAutoPatch_RunWorkerCompleted;
+                bgWorkerAutoPatch.RunWorkerAsync();
             } else
             {
                 MetroMessageBox.Show(this, "Invalid URL of Path List", "Auto Patch for ConquerLoader", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Information);
             }
         }
 
+        private void BgWorkerAutoPatch_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            if (AnyPatchApplied)
+            {
+                lblStatus.Text = "Patched client. You can launch now.";
+            } else
+            {
+                lblStatus.Text = "All ready for launch.";
+            }
+            btnPlay.Enabled = true;
+        }
+
+        private void BgWorkerAutoPatch_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            lblCurrentVer.Text = "Current version: " + AutoPatchConfig.CurrentVersion.ToString();
+            pBarProgress.Value = e.ProgressPercentage;
+            lblStatus.Text = "Patching... " + e.ProgressPercentage + "%";
+        }
+
+        private void BgWorkerAutoPatch_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            // Download the patchlist from specified url
+            client.DownloadFile(AutoPatchConfig.PatchListUrl, "PatchList.json");
+            Uri PatchListURI = new Uri(AutoPatchConfig.PatchListUrl);
+            PatchList = JsonConvert.DeserializeObject<PatchList>(File.ReadAllText("PatchList.json"));
+            if (PatchList.CurrentVersion > AutoPatchConfig.CurrentVersion)
+            {
+                List<Patch> patchs = PatchList.Paths.Where(x => x.Version > AutoPatchConfig.CurrentVersion).OrderBy(x => x.Version).ToList();
+                List<string> FilesToClear = new List<string>();
+                int xCount = 1;
+                foreach (Patch p in patchs)
+                {
+                    string fileNameRar = "Patch" + p.Version + ".rar";
+                    client.DownloadFile(PatchListURI.Scheme + "://" + PatchListURI.Host + "/" + p.RelativeURL, fileNameRar);
+                    if (File.Exists(fileNameRar))
+                    {
+                        var archive = ArchiveFactory.Open(fileNameRar);
+                        foreach (var entry in archive.Entries)
+                        {
+                            if (!entry.IsDirectory)
+                            {
+                                entry.WriteToDirectory(Environment.CurrentDirectory, new ExtractionOptions() { ExtractFullPath = true, Overwrite = true });
+                            }
+                        }
+                        ApplyNewVersionAndReloadVersion();
+                        FilesToClear.Add(fileNameRar);
+                        archive.Dispose();
+                    }
+                    decimal val = Decimal.Divide(xCount, patchs.Count) * 100;
+                    if (val > 100)
+                    {
+                        val = 100;
+                    }
+                    xCount++;
+                    AnyPatchApplied = xCount>1;
+                    bgWorkerAutoPatch.ReportProgress((int)val);
+                }
+                foreach (string filename in FilesToClear)
+                {
+                    File.Delete(filename);
+                }
+            }
+            client.Dispose();
+        }
+
         private void ApplyNewVersionAndReloadVersion()
         {
             AutoPatchConfig.CurrentVersion++;
             File.WriteAllText("AutoPatchPluginCLConfig.json", JsonConvert.SerializeObject(AutoPatchConfig, Formatting.Indented));
-            lblCurrentVer.Text = "Current version: " + AutoPatchConfig.CurrentVersion.ToString();
         }
 
         private bool ValidURL(string URI)
         {
             return Uri.TryCreate(URI, UriKind.Absolute, out Uri uriResult) && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+        }
+
+        private void BtnPlay_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
         }
     }
 }
