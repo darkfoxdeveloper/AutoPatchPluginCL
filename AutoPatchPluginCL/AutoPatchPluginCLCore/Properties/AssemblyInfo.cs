69eaf253356e47105a603a552bf6327d165b0769
DaRkFoxDeveloper <darkfoxdeveloper@gmail.com>
2021-04-05 01:43:28 +0200

    Changes for compatibility CL v1.0.0.5

--- AutoPatchPluginCL/AutoPatchPluginCLCore/Properties/AssemblyInfo.cs
+++ AutoPatchPluginCL/AutoPatchPluginCLCore/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
 [assembly: AssemblyTitle("AutoPatchPluginCLCore")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("AutoPatch Core for ConquerLoader")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("DaRkFoxDeveloper")]
 [assembly: AssemblyProduct("AutoPatchPluginCLCore")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © DaRkFoxDeveloper 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
 // utilizando el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.5")]
+[assembly: AssemblyFileVersion("1.0.0.5")]
